410b5b91f4fc2a0684d610b7e6f9d978ae40170c
Sean King <r.sean.king@gmail.com>
2016-01-23 16:18:00 -0500

    Introduce tests for destructing simple and complex objects

--- test/destructing_spec.js
+++ test/destructing_spec.js
@@ -3,7 +3,7 @@
 const assert = require('assert');
 
 describe('ES2015 Destructing ', () => {
-  describe('arrays', () => {
+  describe('Arrays', () => {
     it('should fully destruct an array', () => {
       // Given
       const array = [1, 2, 3];
@@ -27,6 +27,32 @@ describe('ES2015 Destructing ', () => {
       // Then
       assert.equal(first, array[0]);
       assert.equal(third, array[2]);
+    });
+  });
+
+  describe('Objects', () => {
+    it('should destruct a simple object', () => {
+      // Given
+      const json = { one: 1, two: 2, three: 3 };
+
+      // When
+      const { one, two, three } = json;
+
+      // Then
+      assert.equal(one, json.one);
+      assert.equal(two, json.two);
+      assert.equal(three, json.three);
+    });
+
+    it('should destruct a complex object', () => {
+      // Given
+      const json = { numbers: { one: 1, two: 2, three: 3 } };
+
+      // When
+      const { numbers: { one } } = json;
+
+      // Then
+      assert.equal(one, json.numbers.one);
     });
   });
 });
